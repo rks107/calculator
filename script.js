6580450f9b97ebddb24550b247b5551fd28cc1da
rohit kumar <rohit1071998@gmail.com>
2020-04-30 09:13:40 +0530

    operator functionalty added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var display = document.getElementById("display");
 var operand1 = 0;
 var operand2 = null;
 var operator = null;
-
+display.innerHTML = "0";
 for(var i = 0; i < buttons.length ; i++)
 {
     buttons[i].addEventListener('click', function(){
@@ -11,29 +11,60 @@ for(var i = 0; i < buttons.length ; i++)
 
         if(value == '+'){
 
+            
             operator = '+';
             operand1 = parseFloat(display.textContent);
             display.innerHTML = "";
 
         }else if(value == '-'){
-
             operator = '-';
             operand1 = parseFloat(display.textContent);
             display.innerHTML = "";
 
-        }else if(value == 'ac'){
-
+        }else if(value == '*'){
+            operator = '*';
+            operand1 = parseFloat(display.textContent);
             display.innerHTML = "";
 
-        }else if(value == '='){
+        }else if(value == '/'){
 
+            operator = '/';
+            operand1 = parseFloat(display.textContent);
+            display.innerHTML = "";
+
+        }else if(value == '%'){ 
+            operator = '%';
+            operand1 = parseFloat(display.textContent);
+            operand1 = operand1 / 100;
+            display.innerHTML =  operand1;
+
+        }else if(value == 'pm'){
+            var temp = parseFloat(display.textContent);
+            display.innerHTML = -temp;
+
+        }else if(value == 'ac'){
+
+            display.innerHTML = "0";
+
+        }else if(value == '='){
+            if(operator == '%')
+            {
+                display.innerHTML = operand1;
+
+            }else{
             operand2 = parseFloat(display.textContent);
             var sum = eval(operand1 + " " + operator + " " + operand2);
             display.innerHTML = sum;
+            }
 
         }else{
+            if(display.innerHTML == 0){
+                display.innerHTML = value;
 
-            display.innerHTML +=value;
+            }else{
+                display.innerHTML +=value;
+
+            }  
         }
 
 
